08c76f578537234ce987951f4e31506935e57a5d
manikantaaratikatla <109816879+manikantaaratikatla@users.noreply.github.com>
2025-03-20 10:12:53 +0530

    Create Array2D.java

---
+++ Array2D.java
@@ -0,0 +1,208 @@
+package myFirstProgram;
+import java.util.*;
+public class Array2D
+{
+public static void main(String[] args) 
+{
+				 Random r=new Random();
+				 int arr[][]=new int[3][3];
+				 int i;
+				 int j;
+				 /*
+				 for(i=0;i<arr.length;i++) 
+				 {
+					 for(j=0;j<arr.length;j++) 
+					 {
+						 arr[i][j]=r.nextInt(20);
+					 }
+					 
+				 }
+				 for(i=0;i<arr.length;i++) 
+				 {
+					 for(j=0;j<arr.length;j++) 
+					 {
+						 if(i>j) {
+							 System.out.print("0"+" ");
+						 }
+						 else {
+							 System.out.print(arr[i][j]+" ");
+						 }
+					 }
+					System.out.println();		
+				 */
+
+			
+				 
+				/*
+				 for(i=0;i<arr.length;i++) 
+				 {
+					 for(j=0;j<arr.length;j++) 
+					 {
+						 arr[i][j]=r.nextInt(20);
+					 }
+					 
+				 }
+				 for(i=0;i<arr.length;i++) 
+				 {
+					 for(j=0;j<arr.length;j++) 
+					 {
+						 if(i<j) {
+							 System.out.print("0"+" ");
+						 }
+						 else {
+							 System.out.print(arr[i][j]+" ");
+						 }
+					 }
+					System.out.println();		
+			*/
+
+			
+				 /*
+				 int sum=0;
+				 for(int i=0;i<arr.length;i++) 
+				 {
+					 for(int j=0;j<arr.length;j++) 
+					 {
+						 arr[i][j]=r.nextInt(20);
+					 }
+				 }
+				 for(int i=0;i<arr.length;i++) 
+				 {
+					 for(int j=0;j<arr.length;j++) 
+					 {
+						System.out.print(arr[i][j]+" ");
+					 }
+					System.out.println();
+				 }
+				 for(int i=0;i<arr.length;i++) 
+				 {
+					 for(int j=0;j<arr.length;j++) 
+					 {
+						 if(i==j) {
+							sum=sum+ arr[i][j];
+						 }
+					 }	
+				 }
+				 System.out.println("The diagnal sum is:"+sum);
+			 
+
+			*/
+				 /*
+				
+				 for(int i=0;i<arr.length;i++) 
+				 {
+					 for(int j=0;j<arr.length;j++) 
+					 {
+						 arr[i][j]=r.nextInt(20);
+					 }
+				 }
+				 for(int i=0;i<arr.length;i++) 
+				 {
+					 for(int j=0;j<arr.length;j++) 
+					 {
+						System.out.print(arr[i][j]+" ");
+					 }
+					System.out.println();
+				 }
+				 System.out.println("The Row Sum Is:");
+				 for(int i=0;i<arr.length;i++) 
+				 {	 int sum=0;
+					 for(int j=0;j<arr.length;j++) 
+					 {
+							sum=sum+ arr[i][j];
+					 }	
+					 System.out.println(sum);
+				 }
+				
+			 }
+*/
+			 
+				/*
+				 for(int i=0;i<arr.length;i++) 
+				 {
+					 for(int j=0;j<arr.length;j++) 
+					 {
+						 arr[i][j]=r.nextInt(20);
+					 }
+				 }
+				 for(int i=0;i<arr.length;i++) 
+				 {
+					 for(int j=0;j<arr.length;j++) 
+					 {
+						System.out.print(arr[i][j]+" ");
+					 }
+					System.out.println();
+				 }
+				 System.out.println("The Column Sum Is:");
+				 for(int i=0;i<arr.length;i++) 
+				 {	 int sum=0;
+					 for(int j=0;j<arr.length;j++) 
+					 {
+							sum=sum+ arr[j][i];
+					 }	
+					 System.out.println(sum);
+				 }	
+			 }
+
+		*/
+				 /*
+				 
+				 for(int i=0;i<a.length;i++) 
+				 {
+					 for(int j=0;j<a.length;j++) 
+					 {
+						 a[i][j]=r.nextInt(20);
+					 }
+				 } 
+				 System.out.println("The arrrix A is:");
+				 for(int i=0;i<a.length;i++) 
+				 {
+					 for(int j=0;j<a.length;j++) 
+					 {
+						System.out.print(a[i][j]+" ");
+					 }
+					System.out.println();
+				 }
+				 int b[][]=new int[3][3];
+				 for(int i=0;i<b.length;i++) 
+				 {
+					 for(int j=0;j<b.length;j++) 
+					 {
+						 b[i][j]=r.nextInt(20);
+					 }
+				 } 
+				 System.out.println("The arrrix B is:");
+				 for(int i=0;i<b.length;i++) 
+				 {
+					 for(int j=0;j<b.length;j++) 
+					 {
+						System.out.print(b[i][j]+" ");
+					 }
+					System.out.println();
+				 }
+				 int c[][]=new int[3][3];
+				 for(int i=0;i<c.length;i++) 
+				 {
+					 for(int j=0;j<c.length;j++) 
+					 {
+						c[i][j]=a[i][j]+b[i][j];
+					 }
+				 }
+				 System.out.println("The Addition Of Two arrrix is:");
+				 for(int i=0;i<c.length;i++) 
+				 {
+					 for(int j=0;j<c.length;j++) 
+					 {
+						System.out.print(c[i][j]+" ");
+					 }
+					System.out.println();
+				 }
+			 }
+
+			
+}
+*/
+}
+		}
+
+
